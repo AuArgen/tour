22125927a4fa4a2795d8a7565b3a880967bc34bc
AuArgen <argenazizbekuulu3@gmail.com>
2023-06-19 10:20:27 +0600

    header left

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <meta property="og:title" content="Megaline ноутбуки  года купить —  купить по низкой цене на Megaline Маркете <?php echo $infoMeta; ?> ">
     <meta name="description" content="Купить samsung, lenovo, asus, mac book ноутбуки 2020 года купить - 156 предложений - низкие цены, экспресс-доставка от 1-2 часов, возможность оплаты в рассрочку для части товаров, кешбэк <?php echo $infoMeta; ?> ">
     <title><?php echo $title;?></title>
-    <link href="./img/logo2.png" rel="icon" type="image/x-png">
+    <link href="./img/logo.webp" rel="icon" type="image/x-png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="./css/style.css">
    
@@ -29,13 +29,6 @@
 				<a href="" class="instagram" title = "Instagram page">
 					<i class="fab fa-instagram"></i>
 				</a>
-			</div>
-			<div class="logo">
-				<a href="./">
-					<img src="./img/logo2.png">
-				</a>
-			</div> 
-			<div class="sosial sosialRight">
 				<a href="tel:+996703421521" class="phone" title = "Phone number">
 					<i class="fas fa-phone"></i>
 				</a>
@@ -44,8 +37,15 @@
 					<i class="fab fa-tiktok"></i>
 				</a>
 			</div>
+			
+		
 		</div>
 		<div class="header">
+			<div class="logo">
+				<a href="./">
+					<img src="./img/logo.webp">
+				</a>
+			</div>
 			<a href="" class="pageActive">
 				Home
 			</a>
